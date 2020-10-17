6a5301dc6767f9e48e1f6809ad2de89ad8deef17
rui-watanabe <wataru610806@gmail.com>
2020-10-17 13:09:13 +0900

    add line chart

---
+++ src/components/LinePlot.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+const data = {
+  labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  datasets: [
+    {
+      label: "Demo line plot",
+      backgroundColor: "#008080",
+      borderColor: "#7fffd4",
+      pointBorderWith: 10,
+      data: [4, 6, 7, 9, 11, 55, 57],
+    },
+  ],
+};
+
+const LinePlot: React.FC = () => {
+  return (
+    <>
+      <Line data={data} />
+    </>
+  );
+};
+
+export default LinePlot;
