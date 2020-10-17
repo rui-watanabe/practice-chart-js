eb0864d50e2c2cf18669e5adca513cedb52363d9
rui-watanabe <wataru610806@gmail.com>
2020-10-17 14:42:46 +0900

    add material ui button

---
+++ src/components/MaterialUI.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+const MaterialUI: React.FC = () => {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        The Button
+      </Button>
+    </div>
+  );
+};
+
+export default MaterialUI;
