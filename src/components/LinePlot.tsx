8a4195c8b5d1e34bcf7657e0b4dd191de277008e
rui-watanabe <wataru610806@gmail.com>
2020-10-19 18:55:51 +0900

    change lineplot coloer and ata

--- src/components/LinePlot.tsx
+++ src/components/LinePlot.tsx
@@ -7,9 +7,9 @@ const data = {
     {
       label: "Demo line plot",
       backgroundColor: "#008080",
-      borderColor: "#7fffd4",
+      borderColor: "white",
       pointBorderWidth: 10,
-      data: [4, 6, 7, 9, 11, 55, 57],
+      data: [4, 6, 7, 9, 22, 55, 57],
     },
   ],
 };
